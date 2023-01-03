dbf83d6715db50b5ee9ac55b0f186cbf82aee928
Raúl Campos <mail@raulcampos.net>
2023-01-03 17:29:19 +0000

    new commit

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 // import and config modules
-const mySQL = require("mysql2");
-const dotenv = require("dotenv");
-dotenv.config();
+    const mySQL = require("mysql2");
+    const dotenv = require("dotenv");
+    dotenv.config();
 
-const pool = mySQL.createPool({
-    host: process.env.GG_HOST,
-    database: process.env.GG_DATABASE,
-    user: process.env.GG_USER,
-    password: process.env.GG_PASSWORD
-}).promise();
+    const pool = mySQL.createPool({
+        host: process.env.GG_HOST,
+        database: process.env.GG_DATABASE,
+        user: process.env.GG_USER,
+        password: process.env.GG_PASSWORD
+    }).promise();
 
-exports.pool = pool;
+    exports.pool = pool;
